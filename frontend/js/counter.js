9528b46fade20ee67665df5c0d1337caf26282d0
Toba <tobaodumosu1@gmail.com>
2025-03-14 14:09:44 +0000

    fix: Fixed syntax in counter.js

--- frontend/js/counter.js
+++ frontend/js/counter.js
@@ -14,17 +14,21 @@ document.addEventListener('DOMContentLoaded', function() {
     // Set the appropriate API base URL
     const apiBaseUrl = isDev 
         ? 'https://resume-func-dev-jwmugt4mm4bwe.azurewebsites.net/api/'
-        : 'https://resume-func-prd-s4ot3manh2e7g.azurewebsites.net/api/';
-    
+        : 'https://resume-func-prd-s4ot3manh2e7g.azurewebsites.net/api/'
+
+    const getResumeUrl = `${apiBaseUrl}GetResumeCounter`;
+    const updateResumeUrl = `${apiBaseUrl}UpdateResumeCounter`;
+        
     // Function to get the counter value from the API
     async function getCount() {
         try {
+            
             // API URL - update this with your Azure Function URL
             // For local testing, you can use:
             //const apiUrl = 'http://localhost:7071/api/GetResumeCounter';
             
             // Fetch the counter value
-            const response = await fetch(apiBaseUrl);
+            const response = await fetch(getResumeUrl);
             
             // Check if the response is ok
             if (!response.ok) {
@@ -49,13 +53,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // Function to update the counter
     async function updateCount() {
         try {
-            
+            // API URL - update this with your Azure Function URL
+            //const apiUrl = 'https://resume-func-dev-jwmugt4mm4bwe.azurewebsites.net/api/UpdateResumeCounter?';
             
             // For local testing, you can use:
             //const apiUrl = 'http://localhost:7071/api/UpdateResumeCounter';
             
             // Call the update function
-            const response = await fetch(apiBaseUrl);
+            const response = await fetch(updateResumeUrl);
             
             // Check if the response is ok
             if (!response.ok) {
